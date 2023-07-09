96fcd8e14cd83b7046040b0e4659d4257bca2236
RaySilvaP <silvaray355@gmail.com>
2023-07-09 00:40:41 -0300

    Adicionando dados e pre-preenchimento da tabela

---
+++ Horarios/data.js
@@ -0,0 +1,14 @@
+const materias = [
+    {name: "PORTUGUÊS", teacher: 0, rooms: [0]}, 
+    {name: "FUND. COMP.", teacher: 1, rooms: [0]}, 
+    {name: "INGLÊS INST.", teacher: 2, room: [0]}, 
+    {name: "ALGORITMOS", teacher: 3, room: [0]},
+    {name: "LIN.MARCAÇÃO", teacher: 4, room: [0, 1, 2]},
+    {name: "CÁLCULO", teacher: 5, room: [0]}
+];
+
+const professores = ["IGOR", "ANDRÉ", "DANIELA", "FÁBIO GOMES", "DIOGO", "NAIARA"];
+
+const salas = ["Sala 01", "LAB.INF.1", "LAB.INF.4"];
+
+const periodos = [[[0, 0, 1, 2, 2], [3, 3, 1, 1, 1], [3, 3, 3, 4, 4], [5, 5, 5, 3, 3], [4, 4, 5, 5, 5]]];
